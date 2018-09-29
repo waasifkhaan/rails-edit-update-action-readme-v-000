2ed5d6e82b5a7e3dde7a7530e0a7e3bb989f9a35
Wasif Khan <waasif.khaan@gmail.com>
2018-09-29 17:11:52 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,9 @@ class PostsController < ApplicationController
   end 
   
   def update 
+    @post = Post.find(params[:id])
+  @post.update(title: params[:title], description: params[:description])
+  redirect_to post_path(@post)
   end 
 
   # add edit and update methods here
